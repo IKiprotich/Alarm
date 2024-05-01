be24c99fded9fdce714828bf231f68d3ae5de4ab
Ian Kiprotich <kiprotichian254@gmail.com>
2024-05-01 13:06:29 +0300

    Updated alarm.py

--- alarm.py
+++ alarm.py
@@ -6,22 +6,18 @@ from plyer import notification
 import pygame 
 
 #Main
-
 def set_alarm(hour, minute):
     now = datetime.time()
     alarm_time = datetime(now.year, now.month, now.day, hour, minute)
 
     #Need to calculate the time difference until the alarm
-
     Timediff = alarm_time - now 
 
-    #the alarm should wait till its time
-
+    #The alarm should wait till its time
     time.sleep(Timediff.total_seconds())
 
 
     #Notification part 
-
     def play_sound():
         pygame.mixer.init()
         pygame.mixer.music.load("sounds_eat")  
@@ -30,18 +26,7 @@ def set_alarm(hour, minute):
 # Calling the function to play the sound
     play_sound()
 
-
-    '''notification.notify(
-        title = "My Alarm",
-        message = "Wakie wakie! It's time!",
-        app_icon = None,
-        timeout = 60
-    )
-
-    #Notifiaction sound
-    winsound.Beep(1500, 2000)'''
-
-#taking users input
+#Taking users input
 hour = int(input("Enter the hour (0-23): "))
 minute = int(input("Enter the minute (0-59): "))
 
